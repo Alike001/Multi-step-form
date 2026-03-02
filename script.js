ab8ba24cfccbd9fc3bc5bce2f0eb7562806a1137
Ali Hammed <hammedoye10@gmail.com>
2026-03-02 05:51:46 +0100

    Add step indicator

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 const sections = document.querySelectorAll("section");
 const nextButtons =document.querySelectorAll(".next-btn");
 const submitBtn = document.querySelector(".submit-btn");
+const steps = document.querySelectorAll(".step")
 
 let currentStep = 0;
 
-sections[currentStep].classList.add("active")
+function stepIndicator() {
+  steps.forEach((step,index)=> {
+    step.classList.remove("active", "completed")
+    if(index < currentStep) {
+    step.classList.add("completed");
+  } else if (index === currentStep) {
+    step.classList.add("active");
+  }
+  });
+}
 
-nextButtons.forEach(button => {
+sections[currentStep].classList.add("active");
+stepIndicator();
+
+nextButtons.forEach((button)=> {
   button.addEventListener("click", () => {
     sections[currentStep].classList.remove("active")
     currentStep++;
     sections[currentStep].classList.add("active")
-  })
+    stepIndicator();
+  });
 });
 
 submitBtn.addEventListener("click", () => {
   sections[currentStep].classList.remove("active");
   currentStep++;
   sections[currentStep].classList.add("active");
+  stepIndicator();
 });
-
